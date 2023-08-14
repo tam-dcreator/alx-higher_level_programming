ca8f5f74bd69fd10a18febc55a2888ae4b5d6776
tam-dcreator <tam@THE-7TH>
2023-08-14 21:13:31 +0100

    Still Testing code to check

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -2,7 +2,7 @@
 def max_integer(my_list=[]):
     if len(my_list) == 0:
         return None
-    max_value = my_list[0]
+    max_value = 0
     for i in my_list:
         if i > max_value:
             max_value = i
