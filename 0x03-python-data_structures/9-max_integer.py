006507daf8e1a02293e6e5596f3c81ff874c6310
tam-dcreator <tam@THE-7TH>
2023-08-14 21:14:48 +0100

    Modified code to meet checking requirements

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    if len(my_list) == 0:
+    if not my_list:
         return None
-    max_value = 0
+    max_value = my_list[0]
     for i in my_list:
         if i > max_value:
             max_value = i
