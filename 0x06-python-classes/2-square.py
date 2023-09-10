c8657e5f15bc3dee4557d204c2aa8fac9b143d21
tam-dcreator <tam@THE-7TH>
2023-09-10 18:35:44 +0100

    Instantiated size with a default value

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -13,7 +13,7 @@ class Square:
         size(int): The size of the square
 
     """
-    def __init__(self, size):
+    def __init__(self, size=0):
         if type(size) != int:
             raise TypeError("size must be an integer")
         if size < 0:
