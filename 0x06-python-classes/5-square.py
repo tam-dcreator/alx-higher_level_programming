4bd9e0b8448347f8154bcf64654a49a87fadb894
tam-dcreator <tam@THE-7TH>
2023-09-10 22:09:18 +0100

    Created a class Square that prints the square

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,65 @@
+#!/usr/bin/python3
+
+"""
+A Square Module
+"""
+
+
+class Square:
+    """
+    A square class containing a private attribute.
+
+    Args:
+        size(int): The size of the square
+
+    Raises:
+            TypeError: If value isn't an integer
+            ValueError: if value is less than zero
+    """
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    @property
+    def size(self):
+        """int: Get the size of the Square
+
+        The setter sets the size of the Square
+
+        Args:
+            value(int): The new size of the square, value should be >= 0
+
+        Raises:
+            TypeError: If value isn't an integer
+            ValueError: if value is less than zero
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """Function that calculates and returns the area of the square
+
+        Returns:
+            int: The area of the square.
+        """
+        return self.__size ** 2
+
+    def my_print(self):
+        """Function that prints the square using the pound sign"""
+        if self.__size == 0:
+            print()
+        else:
+            for _ in range(self.__size):
+                for _ in range(self.__size):
+                    print("#", end="")
+                print()
