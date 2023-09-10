94faea4e84860d60b3a1d5c5d474c19c948c4687
tam-dcreator <tam@THE-7TH>
2023-09-10 16:16:34 +0100

    Created a class Square that defines a square with a private attribute

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+"""
+A Square Module
+"""
+
+class Square:
+    """
+    A square class containing a private attribute.
+
+    Args:
+        size(int): The size of the square
+
+    """
+    def __init__(self, size):
+        self.__size = size
