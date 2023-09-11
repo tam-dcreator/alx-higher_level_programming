30c04e25e0b857efdb0c72e9a8fcccf1c5fbb3df
tam-dcreator <tam@THE-7TH>
2023-09-12 00:32:10 +0100

    Module with a BaseGeometry class and an area function

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+"""A BaseGeometry Class Module """
+
+
+class BaseGeometry:
+    """An class BaseGeometry
+
+    """
+    def area(self):
+        """A function raising an error
+
+        Raises:
+            Exception: Generic exception
+
+        """
+        raise Exception("area() is not implemented")
