fa1fabf4e8fa6ba98acde0a1de35a972a8ea0934
tam-dcreator <tam@THE-7TH>
2023-09-11 21:49:16 +0100

    A module for a function that returns the list of available attributes of an object

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+"""A lookup function module"""
+def lookup(obj):
+    """A function to get the attributes of an object.
+
+    Args:
+        obj(object): The first parameter
+
+    Returns:
+        list:A list of attributes available to an object
+    """
+    return dir(obj)
