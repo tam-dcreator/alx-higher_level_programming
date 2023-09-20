b8d33fe633102fc62740803157fdff8d30cb9731
tam-dcreator <tam@THE-7TH>
2023-09-20 16:11:48 +0100

    Decided to use a readline version rather than to outrightly read all the text in the file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -11,5 +11,5 @@ def read_file(filename=""):
         filename(str): Path to the file to be read
     """
     with open(filename, "r", encoding="utf-8") as file:
-        content = file.read()
-        print(content)
+        for line in file:
+            print(line, end="")
