12f2fdf9a013c6b9715d94e617ee257f12f714a1
tam-dcreator <tam@THE-7TH>
2023-09-21 15:08:23 +0100

    Trying to find the solution to a requirement error I have no idea whats causing it

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -7,14 +7,14 @@ A script that adds all arguments to a python list and then saves them in a file
 from sys import argv
 
 save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
-load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 filename = "add_item.json"
 a_list = []
 if len(argv) == 1:
     save_to_json_file(a_list, filename)
 else:
-    a_list = load_from_json_file(filename)
+    my_list = load_from_json_file(filename)
     for i in range(1, len(argv)):
-        a_list.append(argv[i])
-    save_to_json_file(a_list, filename)
+        my_list.append(argv[i])
+    save_to_json_file(my_list, filename)
