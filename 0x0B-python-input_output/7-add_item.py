d11e098ea9691188992fb791406065c2d081c3cd
tam-dcreator <tam@THE-7TH>
2023-09-21 17:32:20 +0100

    Still trying to solve the problem

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -12,11 +12,9 @@ if __name__ == "__main__":
         "6-load_from_json_file").load_from_json_file
 
     filename = "add_item.json"
-    a_list = []
-    if len(argv) == 1:
-        save_to_json_file(a_list, filename)
-    else:
+    try:
         a_list = load_from_json_file(filename)
-        for i in range(1, len(argv)):
-            a_list.append(argv[i])
-            save_to_json_file(a_list, filename)
+    except FileNotFoundError:
+        a_list = []
+    a_list.extend(argv[1:])
+    save_to_json_file(a_list, filename)
