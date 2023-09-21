82cd991c0dddc65a410f8e8adb0c67159ec40f4e
tam-dcreator <tam@THE-7TH>
2023-09-21 17:22:26 +0100

    Still trying

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -9,14 +9,14 @@ from sys import argv
 if __name__ == "__main__":
     save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
     load_from_json_file = __import__(
-        '6-load_from_json_file').load_from_json_file
+        "6-load_from_json_file").load_from_json_file
 
     filename = "add_item.json"
     a_list = []
     if len(argv) == 1:
         save_to_json_file(a_list, filename)
     else:
-        my_list = load_from_json_file(filename)
+        a_list = load_from_json_file(filename)
         for i in range(1, len(argv)):
-            my_list.append(argv[i])
-            save_to_json_file(my_list, filename)
+            a_list.append(argv[i])
+            save_to_json_file(a_list, filename)
