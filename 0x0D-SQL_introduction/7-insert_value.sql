137915b6b298ba867d9c71c27f247e4d9ee306ca
tam-dcreator <tam@THE-7TH>
2023-10-19 13:22:23 +0100

    Script that inserts a new row in the table 'first_table'

---
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -0,0 +1,3 @@
+-- Script that inserts into a new row in the table `first_table`
+-- database would be passed as an argument of the mysql command
+INSERT INTO first_table VALUES (89, "Best School");
