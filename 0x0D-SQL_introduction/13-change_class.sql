d18b1c4da17df53a30633b431d3e8f1a3e22a10d
tam-dcreator <tam@THE-7TH>
2023-10-19 14:45:45 +0100

    Script that removes all records with a score <= 5 in the table second_table

---
+++ 0x0D-SQL_introduction/13-change_class.sql
@@ -0,0 +1,3 @@
+-- Script that removes all records with a score <= 5 in the table 'second_table'
+-- Database name will be passed as an argument of the mysql command
+DELETE FROM second_table WHERE score <= '5';
