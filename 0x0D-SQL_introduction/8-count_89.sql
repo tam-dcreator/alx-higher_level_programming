3da9e5a2138ffedaa2ae1c455277573b36dff9de
tam-dcreator <tam@THE-7TH>
2023-10-19 13:29:09 +0100

    Script that displays the number of records with 'id = 89'

---
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -0,0 +1,3 @@
+-- Script that displays the number of records with `id = 89` in `first_table`
+-- Database name will be passed as an argument of the mysql command
+SELECT COUNT(*) FROM first_table WHERE id = '89';
