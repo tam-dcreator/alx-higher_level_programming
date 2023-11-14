99ae90deeff8e41293ef2da779f1a88e42a4a5ba
tam-dcreator <tam@THE-7TH>
2023-11-14 05:12:13 +0100

    Modified script

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 // Script that prints specific text depending on number of command line
 // arguments passed.
-const { argv } = require('node:process');
-if (argv.length === 3) {
+const argVar = process.argv;
+if (argVar.length === 3) {
   console.log('Argument found');
-} else if (argv.length > 2) {
+} else if (argVar.length > 2) {
   console.log('Arguments found');
 } else {
   console.log('No argument');
