86635a7077c62eb53dee2cbfa123105b38f2cb12
tam-dcreator <tam@THE-7TH>
2023-11-14 05:15:58 +0100

    Modified script, by changing a variable name

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 // Script that prints the value of one argument if present
-const { argv } = require('node:process');
-if (argv[2] == null) {
+const argVar = process.argv;
+if (argVar[2] == null) {
   console.log('No argument');
 } else {
-  console.log(argv[2]);
+  console.log(argVar[2]);
 }
