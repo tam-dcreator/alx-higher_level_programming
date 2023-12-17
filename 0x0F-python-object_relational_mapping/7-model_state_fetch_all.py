e773acf90b8f08694d09b319ab5f256197d8af4f
tam-dcreator <tam@THE-7TH>
2023-12-18 00:47:25 +0100

    Fixed a pycodestyle requirement

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -7,16 +7,16 @@ if __name__ == "__main__":
     from sqlalchemy.orm import Session
     from sqlalchemy import create_engine
     from sys import argv
-    
+
     # Create a new engine for connection
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
                            .format(argv[1], argv[2], argv[3]),
                            pool_pre_ping=True)
-    
+
     # Create a session using the engine
     session = Session(bind=engine)
-    
+
     result = session.query(State).order_by(State.id).all()
-    
+
     for obj in result:
         print(obj.id, obj.name, sep=": ")
