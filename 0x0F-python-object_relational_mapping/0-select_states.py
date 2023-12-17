54dd045991bf726189726d00ae0436f753692581
tam-dcreator <tam@THE-7TH>
2023-12-17 14:46:24 +0100

    A Script that lists all states from the database

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+A Module for listing states from a database
+"""
+if __name__ == "__main__":
+
+    from sys import argv
+    import MySQLdb
+
+    # No argument validation needed as per task requirement
+
+    USERNAME = argv[1]
+    PASSWORD = argv[2]
+    DB_NAME = argv[3]
+
+    # Connect to Database
+
+    conn = MySQLdb.connect(host="127.0.0.1", user=USERNAME, password=PASSWORD,
+                           port=3306, database=DB_NAME)
+    cur = conn.cursor()
+
+    # Query Database and print result
+
+    cur.execute("""SELECT * FROM states ORDER BY states.id""")
+
+    for result in cur.fetchall():
+        print(result)
+
+    # Close the connections
+
+    cur.close()
+    conn.close()
