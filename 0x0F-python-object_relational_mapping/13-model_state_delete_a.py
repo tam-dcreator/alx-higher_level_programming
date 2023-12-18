61a9cde168a8c768d126f66ab1396d129790e128
tam-dcreator <tam@THE-7TH>
2023-12-18 01:55:17 +0100

    A script that deletes all objects in the database whose name contain an 'a'

---
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+A Module that contains a script that deletes all entries whose name has an 'a'
+"""
+if __name__ == "__main__":
+    from model_state import Base, State
+    from sqlalchemy.orm import Session
+    from sqlalchemy import create_engine
+    from sys import argv
+
+    # Create a new engine for connection
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(argv[1], argv[2], argv[3]),
+                           pool_pre_ping=True)
+
+    # Create a session using the engine
+    session = Session(bind=engine)
+
+    # Query the session to get obj, filter by 'a'
+    to_delete = session.query(State).filter(State.name.like('%a%')).all()
+
+    for obj in to_delete:
+        session.delete(obj)
+    session.commit()
