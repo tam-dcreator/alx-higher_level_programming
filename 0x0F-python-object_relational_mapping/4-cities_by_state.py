4047cad9b95420ae137e5e18b1a4b94e408e04e6
tam-dcreator <tam@THE-7TH>
2024-02-19 05:08:24 +0100

    Script that lists cities in a database, joined with their respective state

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -9,7 +9,8 @@ if __name__ == "__main__":
     conn = MySQLdb.connect(host="localhost", port=3306, user=av[1],
                            password=av[2], database=av[3])
     cur = conn.cursor()
-    cur.execute("""SELECT * FROM cities ORDER BY cities.id ASC""")
+    cur.execute("""SELECT cities.id, cities.name, states.name FROM cities
+    JOIN states on cities.state_id = states.id ORDER BY cities.id ASC""")
 
     for result in cur.fetchall():
         print(result)
