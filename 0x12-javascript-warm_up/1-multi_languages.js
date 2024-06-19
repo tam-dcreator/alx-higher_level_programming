33d1d3b34ebf57f3cb78ad61d3dcf30cae249a3f
tam-dcreator <tam@THE-7TH>
2024-06-19 19:49:19 +0100

    javascript warmup files

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,5 @@
+#!/usr/bin/node
+// A script that prints multiline text
+console.log('C is fun');
+console.log('Python is cool');
+console.log('JavaScript is amazing');
---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+// A script that prints multiline text
+const arr = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+for (const item of arr) {
+  console.log(item);
+}
