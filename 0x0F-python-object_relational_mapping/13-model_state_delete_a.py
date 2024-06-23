8a1c7cf6eb78b9a3395206a0c5d75f85099c3be6
tam-dcreator <tam@THE-7TH>
2024-06-23 21:33:48 +0100

    Used port 3306

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -4,18 +4,18 @@ A Module that contains a script that deletes all entries whose name has an 'a'
 """
 if __name__ == "__main__":
     from model_state import Base, State
-    from sqlalchemy.orm import sessionmaker
+    from sqlalchemy.orm import Session
     from sqlalchemy import create_engine
     from sys import argv
 
     # Create a new engine for connection
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
                            .format(argv[1], argv[2], argv[3]),
                            pool_pre_ping=True)
 
     # Create a session using the engine
-    Session = sessionmaker(bind=engine)
-    session = Session()
+    # Session = sessionmaker(bind=engine)
+    session = Session(bind=engine)
 
     # Query the session to get obj, filter by 'a'
     to_delete = session.query(State).filter(State.name.like('%a%')).all()
